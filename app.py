b43b2f703002930ed6bcb03434ac7648465ed9a1
Gilbmet254 <gilbmeta@gmail.com>
2026-06-18 18:58:58 +0300

    Fix blueprint registration order

--- app.py
+++ app.py
@@ -1,41 +1,24 @@
-import os
-from flask import Flask, jsonify
+from flask import Flask
 from flask_cors import CORS
 from extensions import db
-from models import Product, Customer, Order
-
-from routes.products import products_bp
-app.register_blueprint(products_bp)
+import os
 
 app = Flask(__name__)
 CORS(app)
 
-# PostgreSQL (Render)
+# config
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
 
+# IMPORT ROUTES (AFTER app is created)
+from routes.products import products_bp
+app.register_blueprint(products_bp)
+
 @app.route("/")
 def home():
     return {"message": "API running"}
 
-@app.route("/products")
-def products():
-    items = Product.query.all()
-    return jsonify([
-        {
-            "id": p.id,
-            "name": p.name,
-            "price": p.price,
-            "category": p.category,
-            "description": p.description,
-            "image": p.image
-        } for p in items
-    ])
-
 with app.app_context():
     db.create_all()
-
-if __name__ == "__main__":
-    app.run(debug=True)
