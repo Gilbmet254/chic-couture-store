55de0f66761f73d2f98fd2a72c6f5a80ee7bcd2c
Gilbmet254 <gilbmeta@gmail.com>
2026-06-18 10:44:31 +0300

    initial commit

---
+++ backend/app.py
@@ -0,0 +1,45 @@
+from flask import Flask, jsonify, request
+import sqlite3
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+def db():
+    conn = sqlite3.connect("store.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+# GET PRODUCTS
+@app.route("/products")
+def products():
+    conn = db()
+    rows = conn.execute("SELECT * FROM products").fetchall()
+    return jsonify([dict(row) for row in rows])
+
+# SIGNUP
+@app.route("/signup", methods=["POST"])
+def signup():
+    data = request.json
+    conn = db()
+    conn.execute(
+        "INSERT INTO customers (name,email,password) VALUES (?,?,?)",
+        (data["name"], data["email"], data["password"])
+    )
+    conn.commit()
+    return {"message": "User created"}
+
+# CREATE ORDER
+@app.route("/order", methods=["POST"])
+def order():
+    data = request.json
+    conn = db()
+    conn.execute(
+        "INSERT INTO orders (customer_id,total,status) VALUES (?,?,?)",
+        (data["customer_id"], data["total"], "Pending")
+    )
+    conn.commit()
+    return {"message": "Order placed"}
+
+if __name__ == "__main__":
+    app.run(debug=True)
